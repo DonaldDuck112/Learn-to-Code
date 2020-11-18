6b51d1818ad66f9fe990a6751f1267b6c0308660
Seppy King <baenningergian@gmail.com>
2020-11-18 15:51:38 +0100

    super beginning

---
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Spielfelder = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+            Console.WriteLine("Let us play Tic Tac Toe!");
+            Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[0], Spielfelder[1], Spielfelder[2]);
+            Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[3], Spielfelder[4], Spielfelder[5]);
+            Console.WriteLine(" {0}  | {1}  | {2}  ", Spielfelder[6], Spielfelder[7], Spielfelder[8]);
+
+            var input = Console.ReadKey(false);
+            Console.WriteLine(input.Key);
+            //var input2 = Console.ReadLine();
+            //Console.WriteLine(input2);
+
+        }
+    }
+}
