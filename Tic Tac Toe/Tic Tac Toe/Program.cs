e5b5c33f4393c86e6cecb888952eb0bad29f6995
Seppy King <baenningergian@gmail.com>
2020-11-25 15:22:03 +0100

    Eingabe ausgewertet

--- Tic Tac Toe/Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -6,7 +6,7 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            char[] Spielfelder = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            char[] Spielfelder = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
             Console.WriteLine("Let us play Tic Tac Toe!");
             Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[0], Spielfelder[1], Spielfelder[2]);
@@ -15,8 +15,19 @@ namespace Tic_Tac_Toe
 
             var input = Console.ReadKey(false);
             Console.WriteLine(input.Key);
+
+            double wert = Char.GetNumericValue(input.KeyChar);
+            Spielfelder[(int)(wert-1)] = 'X';
+
+            Console.WriteLine("Let us play Tic Tac Toe!");
+            Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[0], Spielfelder[1], Spielfelder[2]);
+            Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[3], Spielfelder[4], Spielfelder[5]);
+            Console.WriteLine(" {0}  | {1}  | {2}  ", Spielfelder[6], Spielfelder[7], Spielfelder[8]);
+
+
             //var input2 = Console.ReadLine();
             //Console.WriteLine(input2);
+
 
         }
     }
