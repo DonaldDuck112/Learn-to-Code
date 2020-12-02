974d0ca1880692a933c58590d23ec95c7a36c788
Seppy King <baenningergian@gmail.com>
2020-12-02 15:58:23 +0100

    spieler wechsel

--- Tic Tac Toe/Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Tic Tac Toe/Program.cs
@@ -7,17 +7,46 @@ namespace Tic_Tac_Toe
         static void Main(string[] args)
         {
             char[] Spielfelder = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            int spieler = 1;
 
-            Console.WriteLine("Let us play Tic Tac Toe!");
-            Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[0], Spielfelder[1], Spielfelder[2]);
-            Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[3], Spielfelder[4], Spielfelder[5]);
-            Console.WriteLine(" {0}  | {1}  | {2}  ", Spielfelder[6], Spielfelder[7], Spielfelder[8]);
+            while (true)
+            {
+                Console.WriteLine("Let us play Tic Tac Toe!");
+                Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[0], Spielfelder[1], Spielfelder[2]);
+                Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[3], Spielfelder[4], Spielfelder[5]);
+                Console.WriteLine(" {0}  | {1}  | {2}  ", Spielfelder[6], Spielfelder[7], Spielfelder[8]);
 
-            var input = Console.ReadKey(false);
-            Console.WriteLine(input.Key);
 
-            double wert = Char.GetNumericValue(input.KeyChar);
-            Spielfelder[(int)(wert-1)] = 'X';
+                var input = Console.ReadKey(false);
+                Console.WriteLine(input.Key);
+
+                double wert = char.GetNumericValue(input.KeyChar);
+                if (spieler == 1)
+                {
+                    Spielfelder[(int)(wert - 1)] = 'X';
+                }
+                if (spieler == 2)
+                {
+                    Spielfelder[(int)(wert - 1)] = 'O';
+                }
+                
+
+
+                if (spieler == 1)
+                {
+                    spieler = 2;
+                }
+                else if (spieler == 2)
+                {
+                    spieler = 1;                    
+                }
+                else 
+                {
+                    //error
+                }
+
+                //todo: spieler wechseln
+            }
 
             Console.WriteLine("Let us play Tic Tac Toe!");
             Console.WriteLine("_{0}__|_{1}__|_{2}__", Spielfelder[0], Spielfelder[1], Spielfelder[2]);
